5d1693d410cee656f29594f529240806257adae6
Insanefoam <spartaq123456789@gmail.com>
2021-03-26 21:01:12 +0600

    feat: swagger openapi

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { PORT } from './@constants';
 import { AppModule } from './app.module';
-import { PORT } from './constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { logger: true });
+
+  const config = new DocumentBuilder()
+    .setTitle('AWS Grep API')
+    .setDescription('AWS S3 GREP servise api specification')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   const port = PORT || 3001;
   await app.listen(port);
