e96b083e4ddccf3374b52a25325cfa08f7f1db3f
Insanefoam <spartaq123456789@gmail.com>
2021-03-28 20:24:29 +0600

    fix: remove redutant env

--- src/@constants/index.ts
+++ src/@constants/index.ts
@@ -2,12 +2,4 @@ import { config } from 'dotenv';
 import { env } from 'process';
 config();
 
-export const {
-  FRONTEND_URL,
-  PORT,
-  ELASTIC_SERVER_URL,
-  AWS_BUCKET_NAME,
-  AWS_REGION_NAME,
-  AWS_ACCESS_KEY,
-  AWS_SECRET_ACCESS_KEY,
-} = env;
+export const { FRONTEND_URL, PORT, ELASTIC_SERVER_URL } = env;
